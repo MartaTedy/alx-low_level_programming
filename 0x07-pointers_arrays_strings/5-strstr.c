61dc7163e6f2becc53e086764a8082668ae42a8f
MartaTedy <tewodrosmartha@gmail.com>
2022-03-29 21:23:45 +0300

    alx is fasinating

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,18 +1,22 @@
 /**
- * _strstr - a function that locates a substring.
+ * _strstr - a function that locates a substring
  *
- * @haystack: input string to search for matching substrings
- * @needle: substring to search for
+ * @haystack: input string to search for matching
+ *            substrings
+ * @needle: subtring to search for
  *
- * Return: a pointer to the beginning of the located substring or NULL if substring is not found
- *
- */
+ * Return: a pointer to the beginning
+ *         of the located substring or
+ *         NULL if substring is not found
+*/
 
 char *_strstr(char *haystack, char *needle)
 {
 	/**
-	 * we initialize a helping variable to assist in returning one of our parameters pointers haystack
-	 */
+	 * we initialize a helping variable
+	 * to assist in returning one of
+	 * our parameters pointers haystack
+	*/
 	char *h, *n;
 
 	while (*haystack != '\0')
