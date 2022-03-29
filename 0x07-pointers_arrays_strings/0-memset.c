b8f8d0b1105ba5f28ca3844cc2712f1c13a210e5
MartaTedy <tewodrosmartha@gmail.com>
2022-03-29 19:45:03 +0300

    alx is fasinating

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,23 +1,32 @@
-#include "main.h"
-
 /**
- * _memset -  fills memory with a constant byte.
- * @s: memory space to be filled. 
- * @b: byte to fill with.
- * @n: number of spaces to fill.
- * Return: pointer to memory area s.
- */
+ * _memset - a function that fills
+ *         memory with a constant byte
+ *
+ * @s: input pointer to char type
+ *    represents the pointer to the
+ *    block of memory to fill
+ * @b: input variable of char type
+ *    represents the character to
+ *    fill s
+ * @n: unsigned int variable
+ *    the number of bytes to be filled
+ *
+ * Return: A pointer to the filled memory
+ *         area @s
+*/
 
 char *_memset(char *s, char b, unsigned int n)
 {
+	/**
+	 * declare an unsigned int
+	 * because we are storing a
+	 * value that will always be
+	 * non-negative (zero or positive)
+	*/
 	unsigned int i;
-	
-	i = 0;
-	
-	while (i < n)
-	{
+
+	for (i = 0; i < n; i++)
 		s[i] = b;
-		i++;
-	}
+
 	return (s);
 }
