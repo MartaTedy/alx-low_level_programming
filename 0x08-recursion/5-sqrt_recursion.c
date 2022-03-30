b1f509b31cb6c2335252a4aaf3424dc7f60f2601
MartaTedy <tewodrosmartha@gmail.com>
2022-03-30 23:03:40 +0300

    alx is fasinating

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,27 +1,27 @@
 #include<stdio.h>
 /**
  * _sqrt - function
- * @n: the nnumber
+ * @n: the number
  * @i: the number
  * Return: Always 0
  */
 
 int _sqrt(int n, int i)
 {
-	if (i * i > n)
+	if (i * i >  n)
 		return (-1);
 	if (i * i == n)
-		return (i)
-	else 
+		return (i);
+	else
 		return (_sqrt(n, i + 1));
 }
 
 /**
- * _sqrt_recursion - function
- *
- * @n: the number
- * Return: Always 0.
- */
+* _sqrt_recursion - function
+*
+* @n: the number
+* Return: Always 0.
+*/
 
 int _sqrt_recursion(int n)
 {
