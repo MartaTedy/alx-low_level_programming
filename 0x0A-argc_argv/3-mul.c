f7237b4cbb194955ca2c362f1c1d25d9b2eded03
MartaTedy <tewodrosmartha@gmail.com>
2022-04-03 21:05:09 +0300

    alx is not done yet

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,26 +2,27 @@
 #define UNUSED(x) (void)(x)
 
 /**
- * ft_atoi - function
- *
- * @str: the chaine
- * Return: Always 0.
- */
+* ft_atoi - function
+*
+* @str: the chaine
+* Return: Always 0.
+*/
 
-int              ft_atoi(const char *str)
+int		ft_atoi(const char *str)
 {
-	int              n;
-	int              sign;
+	int		n;
+	int		sign;
 
 	n = 0;
-	while (*str == ' ' || *str == '\t' || *str == '\v' || *str == '\r' || *str == '\n' || *str == '\f')
+	while (*str == ' ' || *str == '\t' || *str == '\v' || *str == '\r' ||
+			*str == '\n' || *str == '\f')
 		str++;
 	sign = *str == '-' ? -1 : 1;
 	if (sign == -1 || *str == '+')
 		str++;
 	while (*str)
 	{
-		if (*str >= '0' && 'str <= '9')
+		if (*str >= '0' && *str <= '9')
 			n = n * 10 + (*str - '0');
 		else
 			break;
@@ -32,12 +33,12 @@ int              ft_atoi(const char *str)
 }
 
 /**
- * main - function
- *
- * @argv: the chaine of chaine
- * @argc: the int
- * Return: Always 0.
- */
+* main - function
+*
+* @argv: the chaine of chaine
+* @argc: the int
+* Return: Always 0.
+*/
 
 int main(int argc, char **argv)
 {
