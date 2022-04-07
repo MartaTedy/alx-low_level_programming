ad9456136ffa6dd8e41527478e98ac110eb203c1
MartaTedy <tewodrosmartha@gmail.com>
2022-04-07 21:09:02 +0300

    alx is doing fine

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
-#include <main.h>
+#include "main.h"
 
 /**
- * malloc_checked - allocates memory using malloc
+ * *malloc_checked - allocates memory using malloc
  * @b: number of bytes to allocate
  *
  * Return: a pointer to the allocated memory
  */
-void *malloc_checked(unsigned int b);
+void *malloc_checked(unsigned int b)
 {
 	void *ptr;
 
