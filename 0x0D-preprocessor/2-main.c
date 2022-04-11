0ce786cdce8ec7e2319c5f1aaaeeaeec45f47a0d
MartaTedy <tewodrosmartha@gmail.com>
2022-04-11 22:19:02 +0300

    alx is running

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,7 +3,8 @@
 /**
  * main - Entry point
  *
- * Description: prints the name of the file it was compiled from, followed by a new line
+ * Description: prints the name of the file it was
+ * compiled from, followed by a new line
  *
  * Return: Always 0 (Success)
  */
