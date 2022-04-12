639359635fb5c03b6352c237e5da1fa28104b7e3
MartaTedy <tewodrosmartha@gmail.com>
2022-04-12 21:17:53 +0300

    alx is running

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,7 +8,7 @@
  * @age: age of the dog
  * @owner: dog owner
  *
- */
+*/
 struct dog
 {
 	char *name;
@@ -17,11 +17,11 @@ struct dog
 };
 
 /**
- * dog_t - Typedef for struct dog
- */
+ * dog_t -Typedef for struct dog
+*/
 typedef struct dog dog_t;
 
-void int_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
