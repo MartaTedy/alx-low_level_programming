fa2bc6a01c847b1f5da2260d16cc73bad3c0fac4
MartaTedy <tewodrosmartha@gmail.com>
2022-04-13 22:40:22 +0300

    alx is rising

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,8 +4,8 @@
  * @name: name to print
  * @f: function pointer that doesn't return anything
  *
- * Return: Always 0 (success)
- */
+ * Return: Always 0 (Success)
+*/
 
 void print_name(char *name, void (*f)(char *))
 {
