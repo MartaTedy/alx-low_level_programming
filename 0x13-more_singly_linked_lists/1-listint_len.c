c6fdbbb8165e7d44cb8c1453b0ad20ca90e22adb
MartaTedy <tewodrosmartha@gmail.com>
2022-04-27 22:51:40 +0300

    alx as usual

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -6,15 +6,15 @@
  * @h: pointer to the first node
  *
  * Return: number of elements
- */
+*/
 size_t listint_len(const listint_t *h)
 {
 	size_t elements = 1;
 
-	/*return 0 as no of element when h is NULL*/
+	/*return 0 as no of elements when h is NULL*/
 	if (h == NULL)
 		return (0);
-	
+
 	while (h->next != NULL)
 	{
 		/*count no of elements*/
